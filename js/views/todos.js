efebaa55dbe6414e534297050e1d213fb03414b8
Nathan Hall <nathan.mh@gmail.com>
2015-03-24 16:03:10 -0500

    delete and complete items

--- js/views/todos.js
+++ js/views/todos.js
@@ -8,17 +8,40 @@ app.TodoView = Backbone.View.extend({
   events: {
     'dblclick label': "edit",
     "keypress .edit": "updateOnEnter",
-    "blur .edit": "close"
+    "blur .edit": "close",
+    'click .toggle': 'toggleCompleted',
+    "click .destroy": "clear"
   },
 
   initialize: function() {
     this.listenTo(this.model, 'change', this.render);
+    this.listenTo(this.model, 'destroy', this.remove);
+    this.listenTo(this.model, 'visible', this.toggleVisible);
   },
 
   render: function() {
     this.$el.html( this.template( this.model.attributes ) );
+
+    this.$el.toggleClass( 'completed', this.model.get('completed') );
+    this.toggleVisible();
+
     this.$input = this.$('.edit');
     return this;
+  },
+
+  toggleVisible: function() {
+    this.$el.toggleClass('hidden', this.isHidden());
+  },
+
+  isHidden: function() {
+    var isCompleted = this.model.get('completed');
+    return(
+      (!isCompleted && app.TodoFilter === "completed") || (isCompleted && app.TodoFilter === "active")
+    );
+  },
+
+  toggleCompleted: function() {
+    this.model.toggle();
   },
 
   edit: function() {
@@ -39,5 +62,9 @@ app.TodoView = Backbone.View.extend({
     if(e.which === ENTER_KEY) {
       this.close();
     }
+  },
+
+  clear: function() {
+    this.model.destroy();
   }
 });
