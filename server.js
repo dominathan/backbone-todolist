98f119b45f725289461916419c32a64dbf4ca19b
Nathan Hall <nathan.mh@gmail.com>
2015-03-26 18:43:39 -0500

    node.js trial

--- server.js
+++ server.js
@@ -10,9 +10,69 @@ var application_root = __dirname,
 //Create server
 var app = express();
 
-//Where to serve static content
-app.use( express.static( path.join( application_root, 'library') ) );
-app.use(bodyParser());
+//Connect to database
+mongoose.connect( 'mongodb://localhost/library_database' );
+
+//Schemas
+var Book = new mongoose.Schema({
+    title: String,
+    author: String,
+    releaseDate: Date
+});
+
+//Models
+var BookModel = mongoose.model( 'Book', Book );
+
+// Configure server
+app.configure( function() {
+    //parses request body and populates request.body
+    app.use( express.bodyParser() );
+
+    //checks request.body for HTTP method overrides
+    app.use( express.methodOverride() );
+
+    //perform route lookup based on url and HTTP method
+    app.use( app.router );
+
+    //Where to serve static content
+    app.use( express.static( path.join( application_root, 'site') ) );
+
+    //Show all errors in development
+    app.use( express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+// Routes
+app.get( '/api', function( request, response ) {
+    response.send( 'Library API is running' );
+});
+
+app.get( '/api/books', function( request, response ) {
+    return BookModel.find( function( err, books ) {
+        if( !err ) {
+            return response.send( books );
+        } else {
+            return console.log( err );
+        }
+    });
+});
+
+app.post( '/api/books', function( request, response ) {
+    var book = new BookModel({
+        title: request.body.title,
+        author: request.body.author,
+        releaseDate: request.body.releaseDate
+    });
+
+    return book.save( function( err ) {
+        if( !err ) {
+            console.log( 'created' );
+            return response.send( book );
+            } else {
+                console.log( err );
+            }
+    });
+});
+
 
 //Start server
 var port = 4711;
